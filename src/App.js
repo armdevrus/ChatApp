ebc03f939b2c0e66a23082e20ba9ab9e2add6fd7
Arman <aisenov1995@gmail.com>
2021-09-26 21:18:03 +0500

    Список чатов сделан. //Только дизайн. Функционала нет.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from "react";
 import {Link, Route, Switch} from "react-router-dom"
 import Registration from "./pages/Registration/Registration";
 import Authorization from "./pages/Authorization/Authorization";
+import ChatList from "./pages/ChatList/ChatList";
 
 function App() {
     return (
         <>
-           <Link to="/registration">Registration</Link>
-            <br/>
-           <Link to="">Authorization</Link>
+            <Link to="" style={{padding:'10px'}}>Authorization</Link>
+            <Link to="/registration" style={{padding:'10px'}}>Registration</Link>
+            <Link to="/chat_list" style={{padding:'10px'}}>Chat_List</Link>
             <Switch>
                 <Route path="/registration">
                     <Registration/>
                 </Route>
-                <Route exact path="">
+                <Route path="/chat_list">
+                    <ChatList/>
+                </Route>
+                <Route path="">
                     <Authorization/>
                 </Route>
             </Switch>
